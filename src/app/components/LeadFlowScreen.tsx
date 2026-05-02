66e30f0b50496da56136538776df5d8956df98e1
aiagentcreator2025-gif <aiagentcreator2025@gmail.com>
2026-05-02 14:40:36 -0400

    Update LeadFlowScreen.tsx

--- src/app/components/LeadFlowScreen.tsx
+++ src/app/components/LeadFlowScreen.tsx
@@ -702,8 +702,8 @@ function HomeScreen({ onSelect }: { onSelect: (v: View) => void }) {
         <p style={{ fontSize:13, color:"#8A8680", fontWeight:300, fontStyle:"italic" }}>Choose your automation flow to configure</p>
       </div>
       {/* Banner */}
-      <div style={{ width:"100%", borderRadius:16, overflow:"hidden", marginBottom:32 }}>
-        <img src="/banner3.png" alt="To the moon" style={{ width:"100%", height:"260", display:"block" }} />
+      <div style={{ width:"100%", borderRadius:16, overflow:"hidden", marginBottom:32, maxHeight:260 }}>
+        <img src="/banner3.png" alt="To the moon" style={{ width:"100%", height:"100%", display:"block", objectFit:"cover", objectPosition:"center" }} />
       </div>
       <div style={{ display:"flex", gap:24, maxWidth:860 }}>
         <ChoiceCard
