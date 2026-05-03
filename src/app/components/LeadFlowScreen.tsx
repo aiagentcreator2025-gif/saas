4fb4a61d389f7e3101e3f9e1de146986358aeeee
aiagentcreator2025-gif <aiagentcreator2025@gmail.com>
2026-05-03 11:35:35 -0400

    Update LeadFlowScreen.tsx

--- src/app/components/LeadFlowScreen.tsx
+++ src/app/components/LeadFlowScreen.tsx
@@ -757,7 +757,7 @@ function HomeScreen({ onSelect }: { onSelect: (v: View) => void }) {
           alt="banner"
           style={{
             position:"absolute", right:0, top:0,
-            height:"100%", width:"90%",
+            height: 220, width: 667,
             objectFit:"contain", objectPosition:"20% center",
           }}
         />
